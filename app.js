723466aa394374228300708147704a330d3e65a6
koryr5995 <122232214+koryr5995@users.noreply.github.com>
2023-02-10 11:11:50 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const shoot = () => {
+    let mario = document.querySelector('.ball1')
+    mario.classList.toggle('basketball')
+}
+
+const bounce = () => {
+    let nick = document.querySelector('.nick')
+    nick.classList.toggle('nicklogo')
+}
+
+const takeoff = () => {
+    let rocket = document.querySelector('.rocket1')
+    rocket.classList.toggle('rocket')
+}
